888cd0bdbc2cb3dafcf3a450c801b17815198c10
Stephen James Broida <87044792+SJBroida@users.noreply.github.com>
2021-09-17 14:02:13 -0400

    Added correct files to correct folder
    Re-uploaded Pomodoro.js and the new FocusBreakTime.js to the correct folder.

--- src/pomodoro/Pomodoro.js
+++ src/pomodoro/Pomodoro.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
+import FocusBreakTime from "./FocusBreakTime.js";
 import PlayPauseStop from "./PlayPauseStop.js";
 import Progress from "./Progress.js";
 import useInterval from "../utils/useInterval";
-import { minutesToDuration } from "../utils/duration"
 
 // These functions are defined outside of the component to insure they do not have access to state
 // and are, therefore more likely to be pure.
@@ -74,112 +74,16 @@ function Pomodoro() {
     isTimerRunning ? 1000 : null
   );
 
-  /**
-   * Called whenever the "+" or "-" focus button is pushed
-   * @param amount 
-   *    the amount to change the duration (positive or negative)
-   */
-  function changeFocus(amount) {
-    if(amount > 0) {
-      // If the amount is greater than zero, make sure it doesn't go over 60
-      const newDuration = Math.min(focusDuration + amount, 60);
-      // Set the new break duration
-      setFocusDuration(newDuration);
-    } else {
-      // If the amount is less than zero, make sure it doesn't go below 5
-      const newDuration = Math.max(5, focusDuration + amount);
-      // Set the new break duration
-      setFocusDuration(newDuration);
-    }
-  }
-
-  /**
-   * Called whenever the "+" or "-" break button is pushed
-   * @param amount 
-   *    the amount to change the duration (positive or negative)
-   */
-  function changeBreak(amount) {
-    if(amount > 0) {
-      // If the amount is greater than zero, make sure it doesn't go over 15
-      const newDuration = Math.min(breakDuration + amount, 15);
-      // Set the new break duration
-      setBreakDuration(newDuration);
-    } else {
-      // If the amount is less than zero, make sure it doesn't go below 1
-      const newDuration = Math.max(1, breakDuration + amount);
-      // Set the new break duration
-      setBreakDuration(newDuration);
-    }
-  }
-
   // *** No conditioning in the Return Area ***
   return (
     <div className="pomodoro">
-      <div className="row">
-        <div className="col">
-          <div className="input-group input-group-lg mb-2">
-            <span className="input-group-text" data-testid="duration-focus">
-              {/* TODO: Update this text to display the current focus session duration */}
-              Focus Duration: {minutesToDuration(focusDuration)}
-            </span>
-            <div className="input-group-append">
-              {/* TODO: Implement decreasing focus duration and disable during a focus or break session */}
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-testid="decrease-focus"
-                onClick={() => changeFocus(-5)}
-                disabled={session}
-              >
-                <span className="oi oi-minus" />
-              </button>
-              {/* TODO: Implement increasing focus duration  and disable during a focus or break session */}
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-testid="increase-focus"
-                onClick={() => changeFocus(5)}
-                disabled={session}
-              >
-                <span className="oi oi-plus" />
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="float-right">
-            <div className="input-group input-group-lg mb-2">
-              <span className="input-group-text" data-testid="duration-break">
-                {/* TODO: Update this text to display the current break session duration */}
-                {/* Solution is to apply minutesToDuration function to break duration */}
-                Break Duration: {minutesToDuration(breakDuration)}
-              </span>
-              <div className="input-group-append">
-                {/* TODO: Implement decreasing break duration and disable during a focus or break session*/}
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-testid="decrease-break"
-                  onClick={() => changeBreak(-1)}
-                  disabled={session}
-                >
-                  <span className="oi oi-minus" />
-                </button>
-                {/* TODO: Implement increasing break duration and disable during a focus or break session*/}
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-testid="increase-break"
-                  onClick={() => changeBreak(1)}
-                  disabled={session}
-                >
-                  <span className="oi oi-plus" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <FocusBreakTime 
+        session={session}
+        focusDuration={focusDuration}
+        setFocusDuration={setFocusDuration}
+        breakDuration={breakDuration}
+        setBreakDuration={setBreakDuration}
+      />
       <PlayPauseStop 
         isTimerRunning={isTimerRunning}
         setIsTimerRunning={setIsTimerRunning}
