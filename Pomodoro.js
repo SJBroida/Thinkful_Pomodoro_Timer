252ee5071a1a7e1b1ab38356b594ee7a5e8d8834
Stephen James Broida <87044792+SJBroida@users.noreply.github.com>
2021-09-17 13:58:17 -0400

    Updated to create a third component
    Separated the Focus Duration and Break Duration buttons and functions to a separate component

---
+++ Pomodoro.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import FocusBreakTime from "./FocusBreakTime.js";
+import PlayPauseStop from "./PlayPauseStop.js";
+import Progress from "./Progress.js";
+import useInterval from "../utils/useInterval";
+
+// These functions are defined outside of the component to insure they do not have access to state
+// and are, therefore more likely to be pure.
+
+/**
+ * Update the session state with new state after each tick of the interval.
+ * @param prevState
+ *  the previous session state
+ * @returns
+ *  new session state with timing information updated.
+ */
+function nextTick(prevState) {
+  const timeRemaining = Math.max(0, prevState.timeRemaining - 1);
+  return {
+    ...prevState,
+    timeRemaining,
+  };
+}
+
+/**
+ * Higher order function that returns a function to update the session state with the next session type upon timeout.
+ * @param focusDuration
+ *    the current focus duration
+ * @param breakDuration
+ *    the current break duration
+ * @returns
+ *  function to update the session state.
+ */
+function nextSession(focusDuration, breakDuration) {
+  /**
+   * State function to transition the current session type to the next session. e.g. On Break -> Focusing or Focusing -> On Break
+   */
+  return (currentSession) => {
+    if (currentSession.label === "Focusing") {
+      return {
+        label: "On Break",
+        timeRemaining: breakDuration * 60,
+      };
+    }
+    return {
+      label: "Focusing",
+      timeRemaining: focusDuration * 60,
+    };
+  };
+}
+
+function Pomodoro() {
+  // Timer starts out paused
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  // The current session - null where there is no session running
+  const [session, setSession] = useState(null);
+
+  // ToDo: Allow the user to adjust the focus and break duration.
+  const [focusDuration, setFocusDuration] = useState(25);
+  const [breakDuration, setBreakDuration] = useState(5);
+
+  /**
+   * Custom hook that invokes the callback function every second
+   *
+   * NOTE: You will not need to make changes to the callback function
+   */
+  useInterval(() => {
+      if (session.timeRemaining === 0) {
+        new Audio("https://bigsoundbank.com/UPLOAD/mp3/1482.mp3").play();
+        return setSession(nextSession(focusDuration, breakDuration));
+      }
+      return setSession(nextTick);
+    },
+    isTimerRunning ? 1000 : null
+  );
+
+  // *** No conditioning in the Return Area ***
+  return (
+    <div className="pomodoro">
+      <FocusBreakTime 
+        session={session}
+        focusDuration={focusDuration}
+        setFocusDuration={setFocusDuration}
+        breakDuration={breakDuration}
+        setBreakDuration={setBreakDuration}
+      />
+      <PlayPauseStop 
+        isTimerRunning={isTimerRunning}
+        setIsTimerRunning={setIsTimerRunning}
+        session={session}
+        setSession={setSession}
+        focusDuration={focusDuration}
+        setFocusDuration={setFocusDuration}
+        breakDuration={breakDuration}
+        setBreakDuration={setBreakDuration}
+      />
+      <Progress 
+        isTimerRunning={isTimerRunning} 
+        session={session} 
+        focusDuration={focusDuration} 
+        breakDuration={breakDuration}
+      />
+      {/* End of Div with className Pomodoro */}
+    </div>
+  );
+}
+
+export default Pomodoro;
